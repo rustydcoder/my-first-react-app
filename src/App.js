8aae66159925e14215ac8d12848e53906b00205e
Emmanuel George <rustydcoder@gmail.com>
2020-06-26 12:22:48 +0100

    get familiar with setState

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: 'Emmanuel'
+    }
+  }
+
+  changeData = () => {
+    const { userName: name } = this.state
+    this.setState({
+      userName: name === 'Emmanuel' ? 'Rusty' : 'Emmanuel'
+    })
+  }
+
+  render = () =>
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        To Do List
+        {this.state.userName}'s To Do List
       </h4>
+      <button className="btn btn-danger ml-auto" onClick={this.changeData}>Change Name</button>
     </div>
-  );
 }
 
 export default App;
